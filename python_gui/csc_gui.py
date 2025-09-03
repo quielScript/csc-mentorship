52ee270f2772fed173b8141d7daa4b4aeb14a9ec
Exequiel A. <137391972+quielScript@users.noreply.github.com>
2025-09-03 08:06:08 +0800

    Add files via upload

---
+++ python_gui/csc_gui.py
@@ -0,0 +1,14 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
